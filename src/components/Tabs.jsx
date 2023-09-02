935a7a9e32b3ac476b15393010fbaf7055e539d2
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-02 07:11:02 +0200

    created Tabs component

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Tabs = () => {
+  return (
+    <nav>
+      <ul>
+        <li>All</li>
+        <li>Laptops</li>
+        <li>Smartphone</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Tabs;
