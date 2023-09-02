1b8821aa31d741392baeb119ce69ac03f20af7d5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-02 07:36:31 +0200

    used framer-motion stuff, used props to display content

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
-    <div>
-      <h2>TEst name</h2>
-      <img src="" alt="" />
-    </div>
+    <motion.div
+      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{ duration: 1 }}
+      layout
+    >
+      <h2>{product.title}</h2>
+      <img src={product.thumbnail} alt={product.title} />
+    </motion.div>
   );
 };
 
