aec39a2c8b2d112d28960f78ae9b996dfebcde0a
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-02 07:26:30 +0200

    created fetchDataAll function, used inside useEffect, save data inside useState vars, pass props to Tabs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
+import Product from './components/Product';
 import Tabs from './components/Tabs';
+
 function App() {
+  const [category, setCategory] = useState('all');
+  const [fetchData, setFetchData] = useState([]);
+
+  useEffect(() => {
+    const fetchDataAll = async () => {
+      let res;
+      if (category !== 'all') {
+        res = await fetch(
+          `https://dummyjson.com/products/category/${category}`
+        );
+      } else {
+        res = await fetch(`https://dummyjson.com/products`);
+      }
+      const data = await res.json();
+      setFetchData(data.products);
+      console.log(data.products);
+    };
+
+    fetchDataAll();
+  }, [category]);
+
+  const handleCategory = (newCategory) => {
+    setCategory(newCategory);
+  };
+
   return (
     <div className="app">
-      <Tabs />
+      <Tabs onSetCategory={handleCategory} />
+      <div className="products">
+        <Product />
+      </div>
     </div>
   );
 }
